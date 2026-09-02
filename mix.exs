95ab67d41074fa64ef4304d49325e5e21fd9b098
woylie <13847569+woylie@users.noreply.github.com>
2026-09-02 07:53:35 +0200

    init deps, mix config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,55 @@
 defmodule ReqCircuitBreaker.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/scoville/req_circuit_breaker"
+
   def project do
     [
       app: :req_circuit_breaker,
-      version: "0.1.0",
-      elixir: "~> 1.20",
+      version: @version,
+      name: "ReqCircuitBreaker",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      description: description(),
+      package: package(),
+      docs: docs(),
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [
+        flags: [
+          :error_handling,
+          :extra_return,
+          :missing_return,
+          :underspecs,
+          :unmatched_returns,
+          :unknown
+        ],
+        # Configure ignore file. The file uses the short format:
+        # mix dialyzer --format ignore_file_strict
+        ignore_warnings: ".dialyzer_ignore.exs",
+        # Fail if the ignore files has unused filters (e.g. after a
+        # dependency fixed an issue)
+        list_unused_filters: true,
+        # Set location of the persistent lookup table
+        plt_local_path: ".plts"
+      ]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+        "coveralls.github": :test,
+        precommit: :test
+      ]
     ]
   end
 
@@ -18,11 +60,55 @@ defmodule ReqCircuitBreaker.MixProject do
     ]
   end
 
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "format",
+        "credo",
+        "coveralls --warnings-as-errors",
+        "dialyzer",
+        "docs --warnings-as-errors"
+      ]
+    ]
+  end
+
+  defp description do
+    "Req plugin that stops sending requests to a failing service"
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "== 1.7.19", only: [:dev, :test], runtime: false},
+      {:dialyxir, "== 1.4.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "== 0.40.3", only: [:dev, :test], runtime: false},
+      {:excoveralls, "== 0.18.5", only: :test},
+      {:fuse, "~> 2.5"},
+      {:makeup_diff, "== 0.1.1", only: :dev, runtime: false},
+      {:plug, "~> 1.0", only: :test},
+      {:req, "~> 0.7"},
+      {:telemetry, "~> 1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => @source_url <> "/blob/main/CHANGELOG.md"
+      },
+      files: ~w(lib mix.exs LICENSE README* CHANGELOG*)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: @version,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 end
